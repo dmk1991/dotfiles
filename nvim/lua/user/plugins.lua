e4c4a311fe5add8f7c25f50da156ee593e642909
David <dmk1991@gmail.com>
2023-07-26 22:37:20 -0400

    Stops which key from processing comma and semicolon

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -243,11 +243,7 @@ use {
   config = function()
     vim.o.timeout = true
     vim.o.timeoutlen = 1000
-    require("which-key").setup {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-    }
+    require('user/plugins/which-key')
   end
 }
 
